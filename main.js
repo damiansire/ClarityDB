cacd94a284abd740751d67da2230c72fbb011aa6
Damian Sire <damiansire@gmail.com>
2025-03-25 13:16:17 +0700

    fix: add window-all-closed event handler for non-macOS platforms

--- main.js
+++ main.js
@@ -16,3 +16,10 @@ const createWindow = () => {
 app.whenReady().then(() => {
   createWindow();
 });
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+fix: add window-all-closed event handler for non-macOS platforms
